56776b70d4c09699d3cc810936ff0f40d90eda3a
laurachebet1997 <rosechebet19971997@gmail.com>
2023-01-05 00:20:11 -0800

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,6 +2,7 @@
 '''A module for working with lockboxes.
 '''
 
+
 def canUnlockAll(boxes):
     '''Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
